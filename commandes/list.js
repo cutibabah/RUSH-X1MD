61e45f54098d520a8ceff82c2388d740cb6bb0a4
cutibabah <dilshanrashmika128@gmail.com>
2024-06-01 15:55:08 +0530

    Create list.js

---
+++ commandes/list.js
@@ -0,0 +1,411 @@
+/** 
+
+𝗥𝗨𝗦𝗛 𝗠𝗗
+
+  𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 (𝗖) 2024.
+ 𝗟𝗶𝗰𝗲𝗻𝘀𝗲𝗱 𝘂𝗻𝗱𝗲𝗿 𝘁𝗵𝗲  𝗠𝗜𝗧 𝗟𝗶𝗰𝗲𝗻𝘀𝗲;
+ 𝗬𝗼𝘂 𝗺𝗮𝘆 𝗻𝗼𝘁 𝘂𝘀𝗲 𝘁𝗵𝗶𝘀 𝗳𝗶𝗹𝗲 𝗲𝘅𝗰𝗲𝗽𝘁 𝗶𝗻 𝗰𝗼𝗺𝗽𝗹𝗶𝗮𝗻𝗰𝗲 𝘄𝗶𝘁𝗵 𝘁𝗵𝗲 𝗟𝗶𝗰𝗲𝗻𝘀𝗲.
+ 𝗜𝘁 𝗶𝘀 𝘀𝘂𝗽𝗽𝗹𝗶𝗲𝗱 𝗶𝗻 𝘁𝗵𝗲 𝗵𝗼𝗽𝗲 𝘁𝗵𝗮𝘁 𝗶𝘁 𝗺𝗮𝘆 𝗯𝗲 𝘂𝘀𝗲𝗳𝘂𝗹.
+ * @𝗽𝗿𝗼𝗷𝗲𝗰𝘁_𝗻𝗮𝗺𝗲 : 𝗥𝗨𝗦𝗛 𝗠𝗗, 𝗮 𝘀𝗶𝗺𝗽𝗹𝗲 𝗮𝗻𝗱 𝗲𝗮𝘀𝘆 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝘂𝘀𝗲𝗿 𝗯𝗼𝘁 
+ * @𝗼𝘄𝗻𝗲𝗿: 𝗥𝗨𝗦𝗛
+ 
+ **/
+
+
+
+
+
+
+
+
+
+
+
+const { france } = require("../framework/france");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+france({ nomCom: 'list',
+    desc: 'To check list',
+    Categorie: 'General',
+    reaction: '👋', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*list!!*\n ```' + 2000 + '``` *
+
+┏━━━━━━━━❂❂━━━━━━━━━✦         ┏━━━━━✦❘༻༺❘✦━━━━━┓                                     〘𝙱𝙴𝙽𝚂-𝙼𝙳  𝑾𝑯𝑨𝑻𝑺𝑨𝑷𝑷 𝑩𝑶𝑻〙                                   ┗━━━━━✦❘༻༺❘✦━━━━━┛                         ━━━━━༻❁༺━━━━━✦
+╔══❖══════❖══╗
+❄||||𝙱𝙴𝙽𝚂-𝙼𝙳||||❄  ╚══❖══════❖══╝ 
+
+
+
+
+╔══❖════════❖══╗        𝙳𝙴𝚅𝙾𝙻𝙾𝙿𝙴𝙳 𝙱𝚈 𝚁𝚄𝚂𝙷    ╚══❖════════❖══╝                                                           ✩̣̣̣̣̣ͯ┄•͙《✧⃝•͙┄✩ͯ•͙͙✦✧⃝•͙͙✦✩ͯ--》✩̣̣̣̣̣ͯ                           〘 *IA* *〙*
+*╏* bot
+*╏* dalle
+*╏* gpt
+*╰═════════════❂* 
+                           〘 *Games* *〙*
+*╏* riddle
+*╏* riddle
+*╰═════════════❂* 
+                           〘 *Download* *〙*
+*╏* insta
+*╏* twitter
+*╏* tiktok
+*╏* mediafire
+*╏* fbdl
+*╏* fbdl2
+*╏* ytmp4
+*╏* ytmp3
+*╏* igdl
+*╏* mediafire
+*╏* fbdl
+*╏* tiktok
+*╏* fbdl2
+*╰═════════════❂* 
+                           〘 *General* *〙*
+*╏* owner
+*╏* developer
+*╏* support
+*╏* poll
+*╏* ping
+*╏* enc
+*╏* test
+*╏* bmw
+*╏* url
+*╏* poll
+*╏* ping
+*╏* uptime
+*╏* ss
+*╏* vv
+*╏* wallpaper1
+*╏* wallpaper2
+*╏* wallpaper3
+*╏* wallpaper4
+*╏* wallpaper5
+*╏* wallpaper6
+*╏* wallpaper7
+*╏* wallpaper8
+*╏* menu
+*╏* alive
+*╏* tempmail
+*╏* vv
+*╏* script
+*╏* sc
+*╏* repo
+*╏* git
+*╰═════════════❂* 
+                           〘 *Hentai* *〙*
+*╏* hwaifu
+*╏* trap
+*╏* hneko
+*╏* bj
+*╏* ass
+*╏* fuck
+*╏* pussy
+*╏* dick
+*╏* porngif
+*╏* pubg
+*╏* hwaifu
+*╏* trap
+*╏* hneko
+*╏* blowjob
+*╏* hwaifu
+*╏* trap
+*╏* hneko
+*╏* bj
+*╏* ass
+*╏* fuck
+*╏* pussy
+*╏* dick
+*╏* porngif
+*╏* pubg
+*╰═════════════❂* 
+                           〘 *Wallpapers* *〙*
+*╏* messi
+*╏* car
+*╏* enemy
+*╏* random
+*╏* dog
+*╏* cr7
+*╏* blackpink
+*╏* bike
+*╏* aesthetic
+*╏* islamic
+*╏* messi
+*╏* car
+*╏* enemy
+*╏* random
+*╏* dog
+*╏* cr7
+*╏* blackpink
+*╏* bike
+*╏* aesthetic
+*╏* islamic
+*╰═════════════❂* 
+                           〘 *WhatsApp* *〙*
+*╏* lastseen
+*╏* online
+*╏* mydp
+*╏* mystatus
+*╏* groupadd
+*╏* privacy
+*╰═════════════❂* 
+                           〘 *User* *〙*
+*╏* fullpp
+*╏* block
+*╏* unblock
+*╏* send
+*╏* fact
+*╏* quotes
+*╏* whois
+*╏* getpp
+*╰═════════════❂* 
+                           〘 *Mods* *〙*
+*╏* tgs
+*╏* crew
+*╏* join
+*╏* jid
+*╏* sudo
+*╏* mention
+*╏* reboot
+*╏* left
+*╏* reboot
+*╰═════════════❂* 
+                           〘 *Group* *〙*
+*╏* left
+*╏* kickall
+*╏* onlyadmin
+*╏* welcome
+*╏* goodbye
+*╏* antipromote
+*╏* antidemote
+*╏* tagall
+*╏* invite
+*╏* promote
+*╏* demote
+*╏* remove
+*╏* add
+*╏* del
+*╏* info
+*╏* antilink
+*╏* antibot
+*╏* group
+*╏* gname
+*╏* gdesc
+*╏* revoke
+*╏* gpp
+*╏* hidetag
+*╏* automute
+*╏* autounmute
+*╏* fkick
+*╏* nsfw
+*╰═════════════❂* 
+                           〘 *OWNER* *〙*
+*╏* ban
+*╏* bangroup
+*╰═════════════❂* 
+                           〘 *Search* *〙*
+*╏* define
+*╏* lyrics
+*╏* play
+*╏* video
+*╏* img
+*╏* lyrics
+*╏* song
+*╏* stickersearch
+*╏* weather
+*╏* yts
+*╏* play
+*╏* video
+*╰═════════════❂* 
+                           〘 *stickcmd* *〙*
+*╏* setcmd
+*╏* delcmd
+*╏* allcmd
+*╏* setcmd
+*╏* delcmd
+*╏* allcmd
+*╰═════════════❂* 
+                           〘 *tts* *〙*
+*╏* dit
+*╏* itta
+*╏* say
+*╏* dit
+*╏* itta
+*╏* say
+*╰═════════════❂* 
+                           〘 *Audio-Edit* *〙*
+*╏* deep
+*╏* bass
+*╏* reverse
+*╏* slow
+*╏* smooth
+*╏* tempo
+*╏* nightcore
+*╰═════════════❂* 
+                           〘 *Image-Edit* *〙*
+*╏* shit
+*╏* wasted
+*╏* wanted
+*╏* trigger
+*╏* trash
+*╏* rip
+*╏* sepia
+*╏* rainbow
+*╏* hitler
+*╏* invert
+*╏* jail
+*╏* affect
+*╏* beautiful
+*╏* blur
+*╏* circle
+*╏* facepalm
+*╏* greyscale
+*╏* joke
+*╰═════════════❂* 
+                           〘 *Conversion* *〙*
+*╏* sticker
+*╏* scrop
+*╏* take
+*╏* write
+*╏* photo
+*╏* trt
+*╰═════════════❂* 
+                           〘 *Fun* *〙*
+*╏* fancy
+*╏* whois
+*╏* rank
+*╏* toprank
+*╰═════════════❂* 
+                           〘 *Recherche* *〙*
+*╏* apk
+*╰═════════════❂* 
+                           〘 *heroku* *〙*
+*╏* setvar
+*╏* getallvar
+*╏* getvar
+*╰═════════════❂* 
+                           〘 *Reaction* *〙*
+*╏* bully
+*╏* cuddle
+*╏* cry
+*╏* hug
+*╏* awoo
+*╏* kiss
+*╏* lick
+*╏* pat
+*╏* smug
+*╏* bonk
+*╏* yeet
+*╏* blush
+*╏* smile
+*╏* wave
+*╏* highfive
+*╏* handhold
+*╏* nom
+*╏* bite
+*╏* glomp
+*╏* slap
+*╏* kill
+*╏* kick
+*╏* happy
+*╏* wink
+*╏* poke
+*╏* dance
+*╏* cringe
+*╰═════════════❂* 
+                           〘 *Weeb* *〙*
+*╏* waifu
+*╏* neko
+*╏* shinobu
+*╏* megumin
+*╏* cosplay
+*╏* couplepp
+*╰═════════════❂* 
+                           〘 *Logo* *〙*
+*╏* hacker
+*╏* sand
+*╏* dragonball
+*╏* naruto
+*╏* didong
+*╏* wall
+*╏* summer
+*╏* neonlight
+*╏* greenneon
+*╏* glitch
+*╏* devil
+*╏* boom
+*╏* water
+*╏* snow
+*╏* transformer
+*╏* thunder
+*╏* harrypotter
+*╏* cat
+*╏* whitegold
+*╏* lightglow
+*╏* thor
+*╏* neon
+*╏* purple
+*╏* gold
+*╏* arena
+*╏* incandescent
+*╏* child
+*╏* typo
+*╏* light
+*╏* steel
+*╏* sunlight
+*╏* scifi
+*╏* magma
+*╏* leaves
+*╏* hacker
+*╏* sand
+*╏* dragonball
+*╏* naruto
+*╏* didong
+*╏* wall
+*╏* summer
+*╏* neonlight
+*╏* greenneon
+*╏* glitch
+*╏* devil
+*╏* boom
+*╏* water
+*╏* snow
+*╏* transformer
+*╏* thunder
+*╏* harrypotter
+*╏* cat
+*╏* whitegold
+*╏* lightglow
+*╏* thor
+*╏* neon
+*╏* purple
+*╏* gold
+*╏* arena
+*╏* incandescent
+*╏* child
+*╏* typo
+*╏* light
+*╏* steel
+*╏* sunlight
+*╏* scifi
+*╏* magma
+*╏* leaves
+*╰═════════════❂* 
+
+            𝔹𝔼ℕ𝕊-𝕄𝔻    𝙿𝙾𝚆𝙴𝚁𝙳 𝙱𝚈 𝚁𝚄𝚂𝙷
+
+*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*list!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
